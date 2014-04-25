fb95c8c36a4768c88b6fa2fa7f6ec3840e503970
Matthew Weier O'Phinney <matthew@zend.com>
2014-04-25 16:09:40 -0500

    Tested basic behaviors of Application
    - Usage message is displayed for no arguments
    - Unmatched route message is displayed when no routes match
    - Dispatches based on matched route, and returns return value from
      dispatched callback

---
+++ test/ApplicationTest.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
+ * @copyright Copyright (c) 2014 Zend Technologies USA Inc. (http://www.zend.com)
+ */
+
+namespace ZFTest\Console;
+
+use PHPUnit_Framework_TestCase as TestCase;
+use Zend\Console\Adapter\AdapterInterface;
+use ZF\Console\Application;
+use ZF\Console\Dispatcher;
+use ZF\Console\Route;
+
+class ApplicationTest extends TestCase
+{
+    public function setUp()
+    {
+        $this->version = uniqid();
+        $this->console = $this->getMock('Zend\Console\Adapter\AdapterInterface');
+        $this->dispatcher = new Dispatcher();
+        $this->application = new Application(
+            'ZFConsoleApplication',
+            $this->version,
+            $this->getRoutes(),
+            $this->console,
+            $this->dispatcher
+        );
+    }
+
+    public function getRoutes()
+    {
+        return array(
+            array(
+                'name'  => 'self-update',
+                'route' => 'self-update',
+                'description' => 'When executed via the Phar file, performs a self-update by querying
+        the package repository. If successful, it will report the new version.',
+                'short_description' => 'Perform a self-update of the script',
+            ),
+            array(
+                'name' => 'build',
+                'route' => 'build <package> [--target=]',
+                'description' => 'Build a package, using <package> as the package filename, and --target
+        as the application directory to be packaged.',
+                'short_description' => 'Build a package',
+                'options_descriptions' => array(
+                    '<package>' => 'Package filename to build',
+                    '--target'  => 'Name of the application directory to package; defaults to current working directory',
+                ),
+                'defaults' => array(
+                    'target' => getcwd(), // default to current working directory
+                ),
+            ),
+        );
+    }
+
+    public function testRunWithEmptyArgumentsShowsUsageMessage()
+    {
+        $this->console->expects($this->exactly(2))
+            ->method('colorize');
+
+        $this->console->expects($this->exactly(8))
+            ->method('writeLine');
+
+        $this->console->expects($this->exactly(4))
+            ->method('write');
+
+        $this->application->run(array());
+    }
+
+    public function testRunThatDoesNotMatchRoutesDisplaysUnmatchedRouteMessage()
+    {
+        $this->console->expects($this->at(0))
+            ->method('write')
+            ->with($this->stringContains('Unrecognized command:'));
+
+        $this->application->run(array('should', 'not', 'match'));
+    }
+
+    public function testRunThatMatchesInvokesCallableForMatchedRoute()
+    {
+        $phpunit = $this;
+        $this->dispatcher->map('self-update', function ($route, $console) use ($phpunit) {
+            $phpunit->assertEquals('self-update', $route->getName());
+            return 2;
+        });
+
+        $this->assertEquals(2, $this->application->run(array('self-update')));
+    }
+}
