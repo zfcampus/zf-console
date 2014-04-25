401bc58fd830f01142742c4f1bdd4ad7a5ec6886
Matthew Weier O'Phinney <matthew@zend.com>
2014-04-25 14:31:38 -0500

    Added testing bootstrap

---
+++ test/Bootstrap.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
+ * @copyright Copyright (c) 2014 Zend Technologies USA Inc. (http://www.zend.com)
+ */
+
+namespace ZFTest\Console;
+
+use RuntimeException;
+
+error_reporting(E_ALL | E_STRICT);
+chdir(__DIR__);
+
+/**
+ * Test bootstrap, for setting up autoloading
+ *
+ * @subpackage UnitTest
+ */
+class Bootstrap
+{
+    public static function init()
+    {
+        static::initAutoloader();
+    }
+
+    protected static function initAutoloader()
+    {
+        $vendorPath = static::findParentPath('vendor');
+
+        if (! is_readable($vendorPath . '/autoload.php')) {
+            throw new RuntimeException('Cannot find Composer autoloader; have you run "composer install"?');
+        }
+
+        include $vendorPath . '/autoload.php';
+    }
+
+    protected static function findParentPath($path)
+    {
+        $dir = __DIR__;
+        $previousDir = '.';
+        while (!is_dir($dir . '/' . $path)) {
+            $dir = dirname($dir);
+            if ($previousDir === $dir) return false;
+            $previousDir = $dir;
+        }
+        return $dir . '/' . $path;
+    }
+}
+
+Bootstrap::init();
