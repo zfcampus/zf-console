9a8dd1842ece5ee9e8d0751a8ba1418d7e7162bd
Matthew Weier O'Phinney <matthew@zend.com>
2014-07-14 10:44:27 -0500

    [#8] Review and defensive coding
    - Explode:
      - Most ZF2 filters allow either a scalar value, or an array of options
        to the constructor. Updated the constructor to allow an array of
        options.
    - Json:
      - Do not use the scream operator; json_decode() doesn't raise warnings
        anyways. Instead, explicitly trigger an E_USER_WARNING if an error
        occurs, before returning the original value.
      - Also, json_decode() indicates that true, false, and null are all
        expected and possible values. Only in the case of null could an
        error occur; in that case, we determine whether or not to trigger a
        warning.
    - QueryString:
      - Use the Json filter instead of json_decode() so that we get warnings
        for invalid data.
      - Applied code calisthenics to foreach loop to remove nesting.

--- src/Filter/Explode.php
+++ src/Filter/Explode.php
@@ -1,28 +1,61 @@
 <?php
-namespace ZF\Console\Filter;
-
 /**
  * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
  * @copyright Copyright (c) 2014 Zend Technologies USA Inc. (http://www.zend.com)
  */
 
+namespace ZF\Console\Filter;
+
 use Zend\Filter\FilterInterface;
 
 class Explode implements FilterInterface
 {
-    private $delimiter;
+    /**
+     * Default delimiter to use if none provided, or invalid specification provided to constructor.
+     *
+     * @var string
+     */
+    protected $defaultDelimiter = ',';
 
-    public function __construct($delimiter=',')
+    /**
+     * Delimiter to use when filtering values
+     *
+     * @var string
+     */
+    protected $delimiter;
+
+    /**
+     * @param null|array|string $delimiter Delimiter to use when exploding a string to an array
+     */
+    public function __construct($delimiter = null)
     {
+        if (is_array($delimiter)) {
+            if (isset($delimiter['delimiter'])) {
+                $delimiter = $delimiter['delimiter'];
+            } elseif (count($delimiter)) {
+                $delimiter = array_shift($delimiter);
+            } else {
+                $delimiter = $this->defaultDelimiter;
+            }
+        }
+
+        if (null === $delimiter
+            || ! is_string($delimiter)
+        ) {
+            $delimiter = $this->defaultDelimiter;
+        }
+
         $this->delimiter = $delimiter;
     }
 
-    /* (non-PHPdoc)
+    /**
      * @see \Zend\Filter\FilterInterface::filter()
+     * @param mixed $value
+     * @return array|mixed $value Returns an array if a string $value was provided
      */
     public function filter($value)
     {
-        if (!is_string($value)) {
+        if (! is_string($value)) {
             return $value;
         }
 
--- src/Filter/Json.php
+++ src/Filter/Json.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * @license   http://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
  * @copyright Copyright (c) 2014 Zend Technologies USA Inc. (http://www.zend.com)
@@ -11,9 +10,10 @@ use Zend\Filter\FilterInterface;
 
 class Json implements FilterInterface
 {
-
-    /* (non-PHPdoc)
+    /**
      * @see \Zend\Filter\FilterInterface::filter()
+     * @param mixed $value
+     * @return mixed $value Returns the results of deserializing a JSON string; if incapable, returns the original value.
      */
     public function filter($value)
     {
@@ -21,12 +21,31 @@ class Json implements FilterInterface
             return $value;
         }
 
-        @$data = json_decode($value, true);
-        if ($data === false) {
+        $data = json_decode($value, true);
+
+        if (null === $data) {
+            $this->reportJsonDeserializationError();
             return $value;
         }
 
         return $data;
     }
 
+    /**
+     * Raise a user warning if a JSON deserialization error occurred
+     */
+    protected function reportJsonDeserializationError()
+    {
+        $error = json_last_error();
+        switch ($error) {
+            case JSON_ERROR_NONE:
+                return;
+            default:
+                trigger_error(
+                    sprintf('Error deserializing JSON (%d)', $error),
+                    E_USER_WARNING
+                );
+                break;
+        }
+    }
 }
